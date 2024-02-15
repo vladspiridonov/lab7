aaa33d688b50d6bc0f79aef118f60748981e499c
vladspiridonov <vlad777.96@mail.ru>
2024-02-16 00:04:08 +0300

    Добавьте файлы проекта.

---
+++ square of triangle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace square_of_triangle
+{
+    class Program
+    {
+        static double GetSquare(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первый треугольник");
+            Console.Write("x:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y:");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("z:");
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            double s1 = GetSquare(x1, y1, z1);
+            Console.WriteLine("второй треугольник");
+            Console.Write("x:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y:");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("z:");
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            double s2 = GetSquare(x2, y2, z2);
+            if (s1>s2)            
+                Console.WriteLine("первый больше");
+            else
+                if(s1<s2)
+                Console.WriteLine("второй больше");
+            else
+                Console.WriteLine("они равны");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ volume and surface square of cube/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace volume_and_surface_square_of_cube
+{
+    class Program
+    {
+        static void CubeSquareVolume (double a, out double s, out double v)
+        {
+            v = a * a * a;
+            s = a * a * 6;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Сторона куба а:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            CubeSquareVolume(a,out s,out v);
+            Console.WriteLine("s = {0:.00}", s);
+            Console.WriteLine("v = {0:.00}", v);
+            Console.ReadKey();
+        }
+    }
+}
